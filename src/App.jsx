9674ac6aa8899b30591721f369f8db46885872c6
rdelaro <rachealdelarosa@yahoo.com>
2023-06-28 15:25:11 -0700

    Puppy Data with useState mid commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
+import { useState } from 'react'  
 import './App.css'
+import {puppyList} from './data.js'
+
 
 function App() {
-
+  const [puppies, setPuppies] = useState(puppyList)
+console.log(puppies)
   return (
     <>
       <div>
-
+      { 
+   puppies.map((puppy) => {
+     return <p>{puppy.name}</p>
+   })
+}
       </div>
     </>
   )
