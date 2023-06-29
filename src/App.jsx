8c2ed931be98bab134462e3b9e6c0ef3ca1b2397
rdelaro <rachealdelarosa@yahoo.com>
2023-06-29 05:21:25 -0700

    Select a Featured Puppy | COMPLETE

--- src/App.jsx
+++ src/App.jsx
@@ -5,15 +5,34 @@ import {puppyList} from './data.js'
 
 function App() {
   const [puppies, setPuppies] = useState(puppyList)
-console.log(puppies)
+  const [featPupId, setFeatPupId] = useState(null)
+// console.table(puppies)
+const featuredPup = puppies.find((pup)=> pup.id === featPupId)
+// console.log(featuredPup)
+
   return (
     <>
       <div>
+      
       { 
    puppies.map((puppy) => {
-     return <p key={puppy.id}>{puppy.name}</p>
+     return <p 
+     onClick={() => { setFeatPupId(puppy.id)}}
+     key={puppy.id}>{puppy.name}</p>
    })
 }
+
+{/* { featPupId && <p>{ featPupId }</p> }  */}
+{featPupId && (
+        <div>
+          <h2>{featuredPup.name}</h2>
+          <ul>
+            <li>Age: {featuredPup.age}</li>
+            <li>Email: {featuredPup.email}</li>
+          </ul>
+        </div>
+      )}
+
       </div>
     </>
   )
